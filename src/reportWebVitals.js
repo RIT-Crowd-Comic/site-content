49e69aa66c666a10126341dc3b36a061132fb7a6
Travis Stodter <tstodter@gmail.com>
2024-06-03 16:05:51 -0400

    asdf

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -2,8 +2,9 @@ const reportWebVitals = async (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import('web-vitals');
     
+    
     getCLS(onPerfEntry);
-    getFID(onPerfEntry);
+    getFID(onPerfEntry); 
     getFCP(onPerfEntry);
     getLCP(onPerfEntry);
     getTTFB(onPerfEntry);
