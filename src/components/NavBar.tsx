9d239064d9495b480cda6a0ae1b3f970023e4025
JustinBaez8 <jb2436@g.rit.edu>
2024-06-06 16:15:07 -0400

    Added Dropdown Navbar
    Responsive dropdown navbar as an option for mobile/non-desktop devices

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,36 @@
 import Link from 'next/link'
+import '../styles/globals.css'
 
 const NavBar = () => {
   return(
-    <nav className="navbar">
-      <a className="navbar-brand" href="#">
+    <nav className="navbar sticky-top navbar-expand-lg border-bottom">
+      <div className="container-fluid">
+      <Link className="navbar-brand" href="#">
           {/* <Image src="media/Crowd_Comic_Favicon.svg" alt="Crowd Comic Favicon"/> */}
-      </a>
-      <ul className="navbar-nav">
-          <li className="nav-item"><Link className="nav-link" id="homeLink" href="index.html">Home</Link></li>
-          <li className="nav-item"><Link className="nav-link" id="teamLink" href="team.html">Team</Link></li>
-          <li className="nav-item"><Link id="comicLink" href="">Comic</Link></li>
-          <li className="nav-item"><button className="login">Login</button></li>
-      </ul>
+          Crowd Comic
+      </Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="offcanvas offcanvas-end" tabIndex={-1} id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-header" id="offcanvasNavbarLabel">Offcanvas</h5>
+        </div>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" id="homeLink" aria-current="page" href="#">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" id="teamLink" href="#">Team</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-Link" id="comicLink" href="#">Comic</Link>
+            </li>
+        </ul>
+      </div>
+      </div>
     </nav>
   )
 }
