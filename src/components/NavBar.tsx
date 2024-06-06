eb29e66143f71dc6fe3231a120a877656deac7d5
JustinBaez8 <jb2436@g.rit.edu>
2024-06-06 16:24:31 -0400

    Added Offcanvas Navbar
    Another option for the navbar that slides in from the right side of the screen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,13 +12,13 @@ const NavBar = () => {
       <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="offcanvas offcanvas-end" tabIndex={-1} id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+      <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div className="offcanvas-header">
           <h5 className="offcanvas-header" id="offcanvasNavbarLabel">Offcanvas</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-      </div>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="offcanvas-body">
+          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li className="nav-item">
               <Link className="nav-link active" id="homeLink" aria-current="page" href="#">Home</Link>
             </li>
@@ -28,7 +28,8 @@ const NavBar = () => {
             <li className="nav-item">
               <Link className="nav-Link" id="comicLink" href="#">Comic</Link>
             </li>
-        </ul>
+          </ul>
+        </div>
       </div>
       </div>
     </nav>
