c9fc5a9ed192bb9d96db359fe7ac603de0934a6b
YuLingDong <yd4846@rit.edu>
2024-06-07 13:24:39 -0400

    Added Filter Button and Setup section layouts in Team Page
    -Adjusted footer grid system
    -Setup sections layout in the team page with divs and spacing.
    -Created Filter button. Will need to add the filter options (year, name, role) and script to show/hide the filter options when the button is clicked

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,33 +1,33 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 import Filter from "@/components/Filter"
 
 const TeamPage = () => {
   return (
     <main>
-      <div>TeamPage</div>
-
       {/* Content */}
-      <div className="content">
+      <div className="content text-center">
         <h1 className="teamTitle">Meet the Creators!</h1>
         <Filter />
         {/* Creator Group */}
-        <div className="creatorGroup" id="2024">
-          <h2 className="chapterTitle">Chapter 2024</h2>
+        <div className="creatorGroup p-3 pb-4 m-4" id="2024">
+          <h2 className="chapterTitle py-1">Chapter 2024</h2>
           {/* Insert JS to create creator panels */}
         </div>
         {/* /Creator Group */}
 
         {/* Professors */}
-        <div className="professors">
-            <div id="prof1">
-              {/* <Image src="" alt="" placeholder="blur"/> */}
+        <div className="professors p-3 container">
+          <div className='row'>
+            <div className="prof-item col" id="prof1">
+              <Image src="/" alt="professor panel" width="300" height="100"/>
             </div>
-            <div id="prof2">
-              {/* <Image src="" alt="" placeholder="blur"/> */}
+            <div className="prof-item col" id="prof2">
+              <Image src="/" alt="professor panel" width="300" height="100"/>
             </div>
-            <div id="prof3">
-              {/* <Image src="" alt="" placeholder="blur"/> */}
+            <div className="prof-item col" id="prof3">
+              <Image src="/" alt="professor panel" width="300" height="100"/>
             </div>
+          </div>
         </div>
         {/* /Professors */}
       </div>
