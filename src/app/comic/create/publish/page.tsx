5128599a4ad067928f95b494f1aad8714c944a5a
vwl2 <vwl1415@rit.edu>
2024-06-10 14:32:26 -0400

    trying to get carousel working

--- src/app/comic/create/publish/page.tsx
+++ src/app/comic/create/publish/page.tsx
@@ -2,34 +2,31 @@ import Image from "next/image";
 
 const Publish = () => {
     return (<>
-        <div className="panel-overview">
-            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div className="carousel-inner">
-                    {/* temp width and height nums */}
-                    <div className="carousel-item active">
-                        <Image className="d-block" src="/" alt="First panel" width={100} height={50} />
-                    </div>
-                    <div className="carousel-item">
-                        <Image className="d-block" src="/" alt="Second panel" width={100} height={50} />
-                    </div>
-                    <div className="carousel-item">
-                        <Image className="d-block" src="/" alt="Third panel" width={100} height={50} />
-                    </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
+        <div id="panel-overview" className="carousel slide" /*data-bs-ride="carousel"*/>
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <Image src="..." className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <Image src="..." className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <Image src="..." className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#panel-overview" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#panel-overview" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
         </div>
         <div className="branch-hooks">
             <div className="branch-hook-controls">
