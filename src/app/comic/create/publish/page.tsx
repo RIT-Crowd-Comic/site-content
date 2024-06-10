bc078eedd850f35fd03e407f01eb8fd8544b3637
vwl2 <vwl1415@rit.edu>
2024-06-10 14:06:50 -0400

    moving around code

--- src/app/comic/create/publish/page.tsx
+++ src/app/comic/create/publish/page.tsx
@@ -9,16 +9,16 @@ const Publish = () => {
                     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                 </ol>
-                <div className="carousel-inner"> 
+                <div className="carousel-inner">
                     {/* temp width and height nums */}
                     <div className="carousel-item active">
-                        <Image className="d-block" src="" alt="First panel" width={100} height={50}/>
+                        <Image className="d-block" src="/" alt="First panel" width={100} height={50} />
                     </div>
                     <div className="carousel-item">
-                        <Image className="d-block" src="" alt="Second panel" width={100} height={50}/>
+                        <Image className="d-block" src="/" alt="Second panel" width={100} height={50} />
                     </div>
                     <div className="carousel-item">
-                        <Image className="d-block" src="" alt="Third panel" width={100} height={50}/>
+                        <Image className="d-block" src="/" alt="Third panel" width={100} height={50} />
                     </div>
                 </div>
                 <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -32,11 +32,12 @@ const Publish = () => {
             </div>
         </div>
         <div className="branch-hooks">
-            <div className="num-branch-hooks">
-                {/* hooks will be inserted here */}
+            <div className="branch-hook-controls">
+                <button className="add-branch-hook">Add Hook</button>
+                <button className="remove-branch-hook">Remove Hook</button>
             </div>
-            <button className="add-branch-hook">Add Hook</button>
-            <button className="remove-branch-hook">Remove Hook</button>
+            <h2>MINIMUM OF 3 TOTAL BRANCHES REQUIRED</h2>
+            <p>You Currently Have __ Hooks Placed.</p>
         </div>
         <button className="publish-btn">Publish</button>
     </>);
