e5839489aa77716a9377c8579b398cd8b0d9290b
Goose <goosetarlowski@gmail.com>
2024-06-10 17:09:33 -0400

    Creating the splash screen and each individual part of it
    Added different svg files for each layer of the splash screen (background, logo and spikes).
    
    Worked with bootstrap's cards to overlay images on top of each other, so the final splash screen does not stretch / mush together when screen resizes.
    
    Screens <=768px have a taller height than other screen sizes, as we determined it would look better for new users.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="hero">
-    <Image src="../images/Crowd_Comic_Logo_BW.svg" alt="Crowd Comic Logo" width={390} height={256} />
+    <div className="hero card">
+    {/* <Image className="image-fluid" src="../images/splash_screen_bg_bw.svg" alt="" width={400} height={256} /> */}
+
+    <img className="heroimg card-img-top" src="../images/splash_screen_bg_bw.svg" alt="" />
+    <div className="card-img-overlay">
+         <img className="rightspikes card-img-top" src="../images/splash_screen_rightspikes_bw.svg" alt="" />
+         {/* </div><div className="card-img-overlay"> */}
+         <img className="leftspikes card-img-top" src="../images/splash_screen_leftspikes_bw.svg" alt="" />
+         {/* </div><div className="card-img-overlay"> */}
+         <img className="splashlogo card-img-top" src="../images/Crowd_Comic_Logo_BW.svg" alt=""></img>
+    </div>
   </div>
   )
 }
