09785fffdaf59a38d382b5b0c52ec660fe2c9bda
vwl2 <vwl1415@rit.edu>
2024-06-10 16:20:39 -0400

    added in comments for explanation & got rid of autoslide

--- src/app/comic/create/publish/page.tsx
+++ src/app/comic/create/publish/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Publish = () => {
     return (<>
-        <div id="panel-overview" className="carousel slide" /*data-bs-ride="carousel"*/>
+        <div id="panel-overview" className="carousel slide">
             <div className="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Panel 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Panel 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Panel 3"></button>
             </div>
             <div className="carousel-inner">
-                {/* temp values for testing purposes, will be filled in with correct uploaded panels through js */}
+                {/* temp values for testing purposes, will be filled in with correct uploaded panels and vals through js */}
                 <div className="carousel-item active">
                     <Image id="first-panel" src="/images/Crowd_Comic_Logo_BW.svg" className="d-block" alt="..." width={400} height={200} />
                 </div>
@@ -17,9 +17,12 @@ const Publish = () => {
                     <Image id="second-panel" src="/images/Crowd_Comic_Logo.svg" className="d-block" alt="..." width={400} height={200} />
                 </div>
                 <div className="carousel-item" id="branch-hook-img" >
-                    <Image id="third-panel" src="/images/Crowd_Comic_Favicon.svg" className="d-block" alt="..." width={400} height={200} useMap="#panel-map"/>
+                    <Image id="third-panel" src="/images/Crowd_Comic_Favicon.svg" className="d-block" alt="..." width={400} height={200} useMap="#panel-map" />
+                    {/* map of img containing clickable areas/sections (areas will be added when user clicks add branch hook and defines a section of the img they want to be a hook,
+                        these will be able to be later removed through remove branch hook button in js) */}
                     <map name="panel-map">
-                        
+                        {/* ex clickable area
+                        <area shape="rect" coords="290,172,333,250" alt="Phone" href="phone.htm" /> */}
                     </map>
                 </div>
             </div>
@@ -39,12 +42,13 @@ const Publish = () => {
             </div>
             <div className="branch-hook-text">
                 <h2>MINIMUM OF 3 TOTAL BRANCHES REQUIRED</h2>
-                {/* temp text, will be updated through js when above buttons are pressed */}
+                {/* starting text, will be updated through js when either add or remove branch hook button is pressed */}
                 <p id="num-hooks">You Currently Have 0 Hooks Placed.</p>
             </div>
         </div>
         <button id="publish-btn">Publish</button>
-    </>);
+    </>
+    );
 }
 
 export default Publish
