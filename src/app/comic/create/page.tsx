9981c86288db4bd4c6ff2cf605098a129230a267
Nick Bryan <70039522+NickBryan159@users.noreply.github.com>
2024-06-11 13:16:34 -0400

    HTML and Placeholder assets for Create Page
    Added:
    - Image form for the dropbox as well as a background for it
    - 3 Placeholder images for the preview section
    - Publish button

--- src/app/comic/create/page.tsx
+++ src/app/comic/create/page.tsx
@@ -1,7 +1,19 @@
-const Create =  () =>{
+import Image from 'next/image'
+
+const Create = () => {
     return (<>
-    
-    </>);
+        <div id="image-dropbox">
+            <input type="image" src="/images/dropboxUI.png" alt="Submit Image"></input>
+        </div>
+        <div id="image-previews">
+            <Image id="first-preview" src="/images/previewPlaceholder1.png" alt="first-preview" width={300} height={200} />
+            <Image id="second-preview" src="/images/previewPlaceholder2.png" alt="second-preview" width={300} height={200} />
+            <Image id="third-preview" src="/images/previewPlaceholder3.png" alt="third-preview" width={300} height={200} />
+        </div>
+        
+        <button id="to-publish-page">Publish Panels</button>
+    </>
+    );
 }
 
-export default Create 
+export default Create
