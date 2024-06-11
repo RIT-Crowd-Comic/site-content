b2a0299be26dfade67428e5d53714c8b8123eaff
Goose <goosetarlowski@gmail.com>
2024-06-11 10:40:01 -0400

    Swapped from img to Image
    Optimization! I swapped the original "img" I used in Hero.tsx to Image, so things should be a bit more optimized.
    
    Made sure everything is responsive and works as it should!

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 
+import logo from "../../public/images/Crowd_Comic_Logo_BW.svg";
+import leftspikes from "../../public/images/splash_screen_leftspikes_bw.svg";
+import rightspikes from "../../public/images/splash_screen_rightspikes_bw.svg"
+import background from "../../public/images/splash_screen_bg_bw.svg";
+
 const Hero = () => {
   return (
     <div className="hero card">
-    {/* <Image className="image-fluid" src="../images/splash_screen_bg_bw.svg" alt="" width={400} height={256} /> */}
+          <Image layout='fill' className="heroimg card-img-top" src={background} alt="" />
 
-    <img className="heroimg card-img-top" src="../images/splash_screen_bg_bw.svg" alt="" />
-    <div className="card-img-overlay">
-         <img className="rightspikes card-img-top" src="../images/splash_screen_rightspikes_bw.svg" alt="" />
-         {/* </div><div className="card-img-overlay"> */}
-         <img className="leftspikes card-img-top" src="../images/splash_screen_leftspikes_bw.svg" alt="" />
-         {/* </div><div className="card-img-overlay"> */}
-         <img className="splashlogo card-img-top" src="../images/Crowd_Comic_Logo_BW.svg" alt=""></img>
-    </div>
-  </div>
+          <div className="card-img-overlay">
+               <Image className="rightspikes card-img-top" src={rightspikes} alt="" />
+               {/* </div><div className="card-img-overlay"> */}
+
+               <Image className="leftspikes card-img-top" src={leftspikes} alt="" />
+               {/* </div><div className="card-img-overlay"> */}
+               
+               <Image className="splashlogo card-img-top" src={logo} alt="" />
+          </div>         
+     </div>
   )
 }
 
