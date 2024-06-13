a95363309f9b025d0a3d86d2d2cfe0d704cc900a
flundeen <89788775+flundeen@users.noreply.github.com>
2024-06-13 15:45:17 -0400

    publish page hook adding functionality
    set up data structure for panel set and created branch adding funcitonality

---
+++ src/app/comic/create/publish/PanelSet.tsx
@@ -0,0 +1,12 @@
+export interface PanelSet{
+    current_panel_set_uuid: string,
+    parent_branch_uuid:string,
+    image_paths: string[],
+    branches: Branch[];
+};
+export interface Branch{
+    panel: number,
+    x: number,
+    y: number,
+    child_branch_uuid: string
+}
---
+++ src/app/comic/create/publish/PublishButton.tsx
@@ -0,0 +1,14 @@
+
+
+"use client"
+const PublishButton = ({ }) => {
+    const onClick = () => {
+        
+    }
+
+    return (
+        <button onClick={onClick} id="publish-btn">Publish</button>
+    );
+}
+
+export default PublishButton
