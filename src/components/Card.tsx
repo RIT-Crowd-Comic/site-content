07cb5a6f1394fef989415846f2382af187bc2a70
JustinBaez8 <jb2436@g.rit.edu>
2024-06-14 11:15:53 -0400

    Editing Card CSS

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,11 +7,11 @@ export default async function Page()
   return (
     <div className="d-flex flex-wrap justify-content-center ">
       {Cards.cards.map(person =>
-        {return <div className={`card border-black border-3 m-1 ${person.year}`} key={person.name}>
+        {return <div className={`card person-panel border-black border-3 m-1 ${person.year}`} key={person.name}>
           <Link href={person.linkedin}>
-            <div className="card-header bg-transparent" id={person.name}>{person.name}</div>
+            <div className="card-header" id={person.name}>{person.name}</div>
             <Image width={200} height={200} src={person.img} className="img-fluid" alt=""></Image>
-            <div className={`card-footer bg-transparent${person.role}`}>{person.role}</div>
+            <div className={`card-footer ${person.role}`}>{person.role}</div>
           </Link>
           </div>}
       )}
