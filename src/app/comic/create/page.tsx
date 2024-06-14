e84821257aef839998678a92b7ec103be1333e68
Nick Bryan <70039522+NickBryan159@users.noreply.github.com>
2024-06-14 09:25:19 -0400

    Test

--- src/app/comic/create/page.tsx
+++ src/app/comic/create/page.tsx
@@ -1,17 +1,18 @@
+'use client';
 import Image from 'next/image'
+import ImagePreview from '@/components/ImagePreview';
+import '../../../styles/createPage.css';
 
 const Create = () => {
     return (<>
-        <div id="image-dropbox">
-            <input type="image" src="/images/dropboxUI.png" alt="Submit Image"></input>
-        </div>
-        <div id="image-previews">
-            <Image id="first-preview" src="/images/previewPlaceholder1.png" alt="first-preview" width={300} height={200} />
-            <Image id="second-preview" src="/images/previewPlaceholder2.png" alt="second-preview" width={300} height={200} />
-            <Image id="third-preview" src="/images/previewPlaceholder3.png" alt="third-preview" width={300} height={200} />
+        <div id="imagePreviews">
+            <ImagePreview panelNum={1}/>
+            <ImagePreview panelNum={2}/>
+            <ImagePreview panelNum={3}/>
         </div>
         
-        <button id="to-publish-page">Publish Panels</button>
+        <button>Continue</button>
+        <a href="./publish/page.tsx">Visit W3Schools</a>
     </>
     );
 }
