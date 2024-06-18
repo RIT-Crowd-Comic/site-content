52149acad24b6d271701dd2b1a63ca513fd9ba32
JustinBaez8 <jb2436@g.rit.edu>
2024-06-18 15:35:11 -0400

    Making Team Images Fill Panel

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,19 +2,24 @@ import Cards from '@/api/cards.json';
 import Image from 'next/image'
 import Link from 'next/link';
 
-export default async function Page()
-{
+export default async function Page() {
   return (
     <div className="d-flex flex-wrap justify-content-center">
-      {Cards.cards.map(person =>
-        {return <div className={`card person-panel border-black border-3 m-1 ${person.year}`} key={person.name}>
-          <Link href={person.linkedin}>
-            <div className="card-header" id={person.name}>{person.name}</div>
-            <Image width={200} height={200} src={person.img} className="img-fluid portrait" alt=""></Image>
-            <div className={`card-footer ${person.role}`}>{person.role}</div>
-          </Link>
-          </div>}
-      )}
+      {Cards.cards.map(person => {
+        return (
+          <div className={`card person-panel border border-black border-3 m-3 ${person.year}`} key={person.name}>
+            <Image width={200} height={200} src={person.img} className="img-fluid portrait card-img w-100 h-100" alt="" />
+            <div className="card-img-overlay">
+              <div className="card-header p-2 rounded-0" id={person.name}>
+                {person.name}
+              </div>
+              <div className={`card-footer p-2 rounded-0 ${person.role}`}>
+                {person.role}
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
