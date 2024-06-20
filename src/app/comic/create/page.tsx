129f76b0f5b0d54984e1e366b955a8eb72f00909
vwl2 <vwl1415@rit.edu>
2024-06-20 12:44:38 -0400

    canvas to png done

--- src/app/comic/create/page.tsx
+++ src/app/comic/create/page.tsx
@@ -3,12 +3,22 @@ import CreateToolsCanvas from '../../../components/CreateToolsCanvas';
 import Link from 'next/link';
 import '../../../styles/createPage.css';
 
+const exportToPNG = () => {
+    // const canvas = document.getElementById('canvas')
+    // const img = canvas.toDataURL('image/png')
+    var canvas = document.getElementById("canvas");
+    var dataURL = canvas.toDataURL("image/png");
+    var newTab = window.open('about:blank', 'image from canvas');
+    newTab.document.write("<img src='" + dataURL + "' alt='from canvas'/>");
+}
+
 const Create = () => {
     return (
-    <>
-        <CreateToolsCanvas/>
-        <Link href="/comic/create/publish">Continue</Link>
-    </>
+        <>
+            <CreateToolsCanvas />
+            <button onClick={exportToPNG}>Export To PNG</button>
+            <Link href="/comic/create/publish">Continue</Link>
+        </>
     );
 }
 
