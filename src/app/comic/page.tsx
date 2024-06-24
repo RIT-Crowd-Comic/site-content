9faa22046af5a4d751894b3085a794c5af241d82
flundeen <89788775+flundeen@users.noreply.github.com>
2024-06-24 14:58:40 -0400

    css work on reading page

--- src/app/comic/page.tsx
+++ src/app/comic/page.tsx
@@ -11,24 +11,27 @@ const thirdPanelImage = "/comic-panels/third_panel.png";
 //set the base trunks to display by default on read
 const Read = () => {
     return (<>
+        <div className="control-bar">
+                <button id="back-button">Back</button>
+                <button id="hook-toggle">Hook</button>
+                <button id="layout-toggle">Layout</button>
+        </div>
         <div id="comic-panels">
             <div className="first-panel">
-                <Image id="first-img" src={firstPanelImage} alt="" width={900} height={600} />
-                <button id="first-branch-hook" className="branch-hook">Branch 1</button>
+                <Image id="first-img" src={firstPanelImage} alt="" width={525} height={350} />
+                <button id="first-branch-hook" className="branch-hook">1</button>
             </div>
             <div className="second-panel">
-                <Image id="second-img" src={secondPanelImage} alt="" width={900} height={600} />
+                <Image id="second-img" src={secondPanelImage} alt="" width={525} height={350} />
             </div>
             <div className="third-panel">
-                <Image id="third-img" src={thirdPanelImage} alt="" width={900} height={600} />
+                <Image id="third-img" src={thirdPanelImage} alt="" width={525} height={350} />
                 <div className="third-panel-container">
-                    <button id="second-branch-hook" className="branch-hook">Branch 2</button>
-                    <button id="third-branch-hook" className="branch-hook"><Link href="/comic/create">Branch 3</Link></button>
+                    <button id="second-branch-hook" className="branch-hook">2</button>
+                    <button id="third-branch-hook" className="branch-hook">3<Link href="/comic/create"></Link></button>
                 </div>
             </div>
         </div>
-        
-        <button id="back-button">Back</button>
     </>);
 }
 
