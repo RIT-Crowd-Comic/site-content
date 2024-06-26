bb3a8eb81544ab56e89dbb20879fb5a04302a3c9
YuLingDong <yd4846@rit.edu>
2024-06-26 10:33:00 -0400

    Added Hover Animation to Our Team button and backToTop button
    The hover animation for Get Started button is commented out for now. It will be un-commented when I figure out how to hide and show the getStarted text after the image is hovered over.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,103 +23,102 @@ export default function Home() {
       <Hero />
       {/* <ScrollToTop /> */}
       <section className="main-section">
-      <div className="container-fluid" id="mission-id">
-        <div className="mission row">
-          <div className="col"></div>
-          <div className="col-auto">
-            <h2>Mission</h2>
-          </div>
-        </div>
-
-        <div className="missionimg row card">
-          <div className="card-img-overlay">
-            <Image className="bottomMission card-img-top" src={bottomMission} alt="Mission Textbox" />
-            <Image className="topMission card-img-top" src={topMission} alt="Mission Textbox" />
-          </div>
-          <div className="card-body">
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eum exercitationem maxime ab eveniet? Ducimus, sunt. Tempore porro, sapiente commodi ipsa veritatis sequi? Inventore quasi placeat fugit, perferendis velit hic, dolorem deleniti saepe at, assumenda similique quis esse atque nobis!
-            </p>
-          </div>
-        </div>
-
-        <div className="whatcanido">
-          <div className="row">
+        <div className="container-fluid" id="mission-id">
+          <div className="mission row">
             <div className="col"></div>
             <div className="col-auto">
-              <h2>What can I do?</h2>
+              <h2>Mission</h2>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="preview">
-        <Image className="panel-one preview-panels" src={firstPanelImage} alt="Preview Panel" />
-        <Image className="preview-arrow" src={previewArrow} alt="Preview Arrow"></Image>
-        <Image className="panel-two preview-panels" src={secondPanelImage} alt="Preview Panel" />
-        <Image className="preview-arrow" src={previewArrow} alt="Preview Arrow"></Image>
-        <Image className="panel-three preview-panels" src={thirdPanelImage} alt="Preview Panel" />
-      </div>
-      <div className="read-and-create">
-        <div className="read">
-          <div className="row">
-            <div className="col-auto">
-              <h2>Read</h2>
-            </div>
-            <div className="col"></div>
-          </div>
-        </div>
-        <div className="readimg row card">
-          <div className="card-img-overlay">
-            <Image className="bottomRead card-img-top" src={bottomRead} alt="Read Textbox" />
-            <Image className="topRead card-img-top" src={topRead} alt="Read Textbox" />
-          </div>
-          <div className="card-body">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
-              Soluta culpa dolorum eius explicabo eum officia magnam!
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
-              Soluta culpa dolorum eius explicabo eum officia magnam!
-            </p>
-          </div>
-        </div>
-        <div className="create">
-          <div className="row">
-            <div className="col">
-            </div>
-            <div className="col-auto">
-              <h2>Create</h2>
-            </div>
-          </div>
-          <div className="createimg row card">
+
+          <div className="missionimg row card">
             <div className="card-img-overlay">
-              <Image className="bottomCreate card-img-top" src={bottomCreate} alt="Create Textbox" />
-              <Image className="topCreate card-img-top" src={topCreate} alt="Create Textbox" />
+              <Image className="bottomMission card-img-top" src={bottomMission} alt="Mission Textbox" />
+              <Image className="topMission card-img-top" src={topMission} alt="Mission Textbox" />
             </div>
             <div className="card-body">
               <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
-                reiciendis?
-                Aliquam reiciendis molestiae cumque pariatur hic qui!
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
-                reiciendis?
-                Aliquam reiciendis molestiae cumque pariatur hic qui!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eum exercitationem maxime ab eveniet? Ducimus, sunt. Tempore porro, sapiente commodi ipsa veritatis sequi? Inventore quasi placeat fugit, perferendis velit hic, dolorem deleniti saepe at, assumenda similique quis esse atque nobis!
               </p>
             </div>
           </div>
 
-          <div className="row">
-            <div className="col">
-            </div>
-            <div className="col-auto">
-              <div className="getStarted">
-                <button><Link href="/comic">Get Started</Link></button>
-                <Image className="comic card-img-top" src={comic} alt="Get Started Button" />
+          <div className="whatcanido">
+            <div className="row">
+              <div className="col"></div>
+              <div className="col-auto">
+                <h2>What can I do?</h2>
               </div>
             </div>
-            <div className="col"></div>
           </div>
         </div>
-      </div>
+        <div className="preview">
+          <Image className="panel-one preview-panels" src={firstPanelImage} alt="Preview Panel" />
+          <Image className="preview-arrow" src={previewArrow} alt="Preview Arrow"></Image>
+          <Image className="panel-two preview-panels" src={secondPanelImage} alt="Preview Panel" />
+          <Image className="preview-arrow" src={previewArrow} alt="Preview Arrow"></Image>
+          <Image className="panel-three preview-panels" src={thirdPanelImage} alt="Preview Panel" />
+        </div>
+        <div className="read-and-create">
+          <div className="read">
+            <div className="row">
+              <div className="col-auto">
+                <h2>Read</h2>
+              </div>
+              <div className="col"></div>
+            </div>
+          </div>
+          <div className="readimg row card">
+            <div className="card-img-overlay">
+              <Image className="bottomRead card-img-top" src={bottomRead} alt="Read Textbox" />
+              <Image className="topRead card-img-top" src={topRead} alt="Read Textbox" />
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
+                Soluta culpa dolorum eius explicabo eum officia magnam!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
+                Soluta culpa dolorum eius explicabo eum officia magnam!
+              </p>
+            </div>
+          </div>
+          <div className="create">
+            <div className="row">
+              <div className="col">
+              </div>
+              <div className="col-auto">
+                <h2>Create</h2>
+              </div>
+            </div>
+            <div className="createimg row card">
+              <div className="card-img-overlay">
+                <Image className="bottomCreate card-img-top" src={bottomCreate} alt="Create Textbox" />
+                <Image className="topCreate card-img-top" src={topCreate} alt="Create Textbox" />
+              </div>
+              <div className="card-body">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
+                  reiciendis?
+                  Aliquam reiciendis molestiae cumque pariatur hic qui!
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
+                  reiciendis?
+                  Aliquam reiciendis molestiae cumque pariatur hic qui!
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+              </div>
+              <div className="col-auto">
+                <div className="getStarted"> 
+                  <Image className="comic card-img-top" src={comic} alt="Get Started Button" />
+                  <button><Link href="/comic">Get Started</Link></button>
+                </div>
+              </div>
+              <div className="col"></div>
+            </div>
+          </div>
+        </div>
       </section>
       <div className="our-team">
         <button><Link href="/team" >Our Team</Link></button>
