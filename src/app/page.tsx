7b45be3e77df0aeaf43f98b2d8e93df126602ef6
YuLingDong <yd4846@rit.edu>
2024-06-26 11:23:50 -0400

    Fixed Merge Errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,98 +38,96 @@ export default function Home() {
               <h2>Mission</h2>
             </div>
           </div>
-
-        <div className="missionimg row card">
-          <div className="card-img-overlay">
-            <Image className="bottomMission card-img-top" src={bottomMission} alt="" />
-            <Image className="topMission card-img-top" src={topMission} alt="" />
-
-            <Image className="leftMission card-img-top" src={desktopLeftMission} alt="" />
-            <Image className="rightMission card-img-top" src={desktopRightMission} alt="" />
-          </div>
-          <div className="card-body">
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eum exercitationem maxime ab eveniet? Ducimus, sunt. Tempore porro, sapiente commodi ipsa veritatis sequi? Inventore quasi placeat fugit, perferendis velit hic, dolorem deleniti saepe at, assumenda similique quis esse atque nobis!
-            </p>
-          </div>
-        </div>
-
-        <div className="whatcanido">
-          <div className="row">
-            <div className="col"></div>
-            <div className="col-auto">
-              <h2>What can I do?</h2>
-            </div>
-          </div>
-        </div>
-        </div>
-        <div className="preview">
-          <Image className="panel-one preview-panels" src={firstPanelImage} alt="" />
-          <Image className="panel-two preview-panels" src={secondPanelImage} alt="" />
-          <Image className="panel-three preview-panels" src={thirdPanelImage} alt="" />
-        </div>
-        <div className="read-and-create">
-          <div className="read">
-            <div className="row">
-              <div className="col-auto">
-                <h2>Read</h2>
-              </div>
-              <div className="col"></div>
-            </div>
-          </div>
-          <div className="readimg row card">
+          <div className="missionimg row card">
             <div className="card-img-overlay">
-              <Image className="bottomRead card-img-top" src={bottomRead} alt="" />
-              <Image className="topRead card-img-top" src={topRead} alt="" />
+              <Image className="bottomMission card-img-top" src={bottomMission} alt="" />
+              <Image className="topMission card-img-top" src={topMission} alt="" />
 
-              <Image className="desktopRead card-img-top" src={desktopRead} alt="" />
+              <Image className="leftMission card-img-top" src={desktopLeftMission} alt="" />
+              <Image className="rightMission card-img-top" src={desktopRightMission} alt="" />
             </div>
             <div className="card-body">
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
-                Soluta culpa dolorum eius explicabo eum officia magnam!
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
-                Soluta culpa dolorum eius explicabo eum officia magnam!
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eum exercitationem maxime ab eveniet? Ducimus, sunt. Tempore porro, sapiente commodi ipsa veritatis sequi? Inventore quasi placeat fugit, perferendis velit hic, dolorem deleniti saepe at, assumenda similique quis esse atque nobis!
               </p>
             </div>
           </div>
-          <div className="create">
+          <div className="whatcanido">
             <div className="row">
-              <div className="col">
-              </div>
+              <div className="col"></div>
               <div className="col-auto">
-                <h2>Create</h2>
+                <h2>What can I do?</h2>
               </div>
             </div>
-            <div className="createimg row card">
-              <div className="card-img-overlay">
-                <Image className="bottomCreate card-img-top" src={bottomCreate} alt="" />
-                <Image className="topCreate card-img-top" src={topCreate} alt="" />
-                <Image className="desktopCreate card-img-top" src={desktopCreate} alt="" />
-              </div>
-              <div className="card-body">
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
-                  reiciendis?
-                  Aliquam reiciendis molestiae cumque pariatur hic qui!
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
-                  reiciendis?
-                  Aliquam reiciendis molestiae cumque pariatur hic qui!
-                </p>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col">
-              </div>
-              <div className="col-auto">
-                <div className="getStarted">
-                  <Image className="comic card-img-top" src={comic} alt="" />
-                  <button><Link href="/comic">Get Started</Link></button>                
+          </div>
+          <div className="preview">
+            <Image className="panel-one preview-panels" src={firstPanelImage} alt="" />
+            <Image className="panel-two preview-panels" src={secondPanelImage} alt="" />
+            <Image className="panel-three preview-panels" src={thirdPanelImage} alt="" />
+          </div>
+          <div className="read-and-create">
+            <div className="read">
+              <div className="row">
+                <div className="col-auto">
+                  <h2>Read</h2>
                 </div>
                 <div className="col"></div>
               </div>
             </div>
+            <div className="readimg row card">
+              <div className="card-img-overlay">
+                <Image className="bottomRead card-img-top" src={bottomRead} alt="" />
+                <Image className="topRead card-img-top" src={topRead} alt="" />
+
+                <Image className="desktopRead card-img-top" src={desktopRead} alt="" />
+              </div>
+              <div className="card-body">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
+                  Soluta culpa dolorum eius explicabo eum officia magnam!
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem explicabo pariatur facere.
+                  Soluta culpa dolorum eius explicabo eum officia magnam!
+                </p>
+              </div>
+            </div>
+            <div className="create">
+              <div className="row">
+                <div className="col">
+                </div>
+                <div className="col-auto">
+                  <h2>Create</h2>
+                </div>
+              </div>
+              <div className="createimg row card">
+                <div className="card-img-overlay">
+                  <Image className="bottomCreate card-img-top" src={bottomCreate} alt="" />
+                  <Image className="topCreate card-img-top" src={topCreate} alt="" />
+                  <Image className="desktopCreate card-img-top" src={desktopCreate} alt="" />
+                </div>
+                <div className="card-body">
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
+                    reiciendis?
+                    Aliquam reiciendis molestiae cumque pariatur hic qui!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae aliquam animi cumque
+                    reiciendis?
+                    Aliquam reiciendis molestiae cumque pariatur hic qui!
+                  </p>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col">
+                </div>
+                <div className="col-auto">
+                  <div className="getStarted">
+                    <Image className="comic card-img-top" src={comic} alt="" />
+                    <button><Link href="/comic">Get Started</Link></button>                
+                  </div>
+                </div>
+                <div className="col"></div>
+                </div>
+              </div>
           </div>
         </div>
       </section>
