0dd30a53b8bc28316cf16bcc402ab001b98bf0f7
YuLingDong <yd4846@rit.edu>
2024-07-03 12:14:02 -0400

    Added Textbox Hover Animation to Team Panels

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,15 +5,22 @@ import Image from 'next/image'
 import Link from 'next/link';
 import React, {useState, useEffect} from 'react';
 
-
 export default function Page() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="d-flex flex-wrap justify-content-center">
       {Cards.cards.map(person => {
         return (
-          <div className={`card person-panel border border-black border-3 m-3 ${person.year}`} key={person.name}>
-              <Image width={200} height={200} src={person.photo} className="img-fluid portrait card-img w-100 h-100" alt="" />
-              <Link href={`${person.linkedin}`}>
+          <div className={`card person-panel border border-black border-3 m-3 ${person.year}`} key={person.name} onMouseOver={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            {/* Show drawing as default, Show photo when hovered */}
+            {/* {isHovered ? ( */}
+              {/* <Image width={200} height={200} src={person.photo} className="img-fluid portrait card-img w-100 h-100" alt="Creator Panel Drawings" />
+            ):( */}
+              <Image width={200} height={200} src={person.img} className="img-fluid portrait card-img w-100 h-100" alt="Creator Photos" />
+            {/* )} */}
+            
+            <Link href={`${person.linkedin}`}>
               <div className="card-img-overlay">
                 <div className="card-header p-2 rounded-0" id={person.name}>
                   {person.name}
@@ -23,7 +30,6 @@ export default function Page() {
                 </div>
               </div>
             </Link>
-            
           </div>
         );
       })}
