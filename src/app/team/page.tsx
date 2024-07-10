a59fd54226af74d7aca279b1815d1ff93dc07818
YuLingDong <yd4846@rit.edu>
2024-07-10 10:55:14 -0400

    merge

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -43,10 +43,10 @@ const TeamPage = () => {
             </Link>
           </div>
           <div className={`${styles.personPanel} card border border-black border-3 m-2`}>
-              <Image width={300} height={300} className={`${styles.portrait} img-fluid card-img w-100 h-100 rounded-0`} alt="" src="/images/people/Sten_Placeholder.jpg"/>
+              <Image width={300} height={300} className={`${styles.portrait} img-fluid card-img w-100 h-100 rounded-0`} alt="Sten McKinzie Drawing" src="/images/people/Sten_Placeholder.jpg"/>
               <Link href="https://www.linkedin.com/in/sten-mckinzie-b33b6414b/">
               <div className="card-img-overlay rounded-0">
-                <Image width={300} height={300} className={`${styles.portrait} ${styles.overlayImg} overlay-img img-fluid card-img w-100 h-100 rounded-0`} alt="Travis Stodter" src="/images/people/photos/Sten_McKinzie.jpg"/>
+                <Image width={300} height={300} className={`${styles.portrait} ${styles.overlayImg} overlay-img img-fluid card-img w-100 h-100 rounded-0`} alt="Sten Mckinzie" src="/images/people/photos/Sten_Mckinzie.jpg"/>
                 <div className={`${styles.nameText} card-header p-2 rounded-0`}>
                   Sten Mckinzie
                 </div>
