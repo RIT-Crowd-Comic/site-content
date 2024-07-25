9497ce548c6f6ee3b13b79567d6b382dfc9cd6b7
apowers131 <arthurpow131@gmail.com>
2024-07-25 15:48:20 -0700

    added util method for calculating polygon area

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,22 @@
 const createSVGPath = (points: number[][]) => points.map((point, i) => (i === 0 ? 'M' : 'L') + `${point[0]} ${point[1]}`).join(' ')
 
 /**
+ * Calculate the area of a polygon
+ * @param points array of points [[x1, y1], [x2, y2]...]
+ */
+
+// NOTE: THIS IS SIGNED AREA. IF YOU WANT NORMAL AREA, WRAP THE CONTENTS OF THIS FUNCTION IN Math.abs()
+const calculateArea = (points: number[][]) =>
+    Math.abs(points.reduce(
+        (area, current, i) =>
+            area +
+            (i < points.length - 1 ? // prevent array out of bounds
+                (current[1] + points[i + 1][1]) * (points[i + 1][0] - current[0]) / 2 :
+                (current[1] + points[0][1]) * (points[0][0] - current[0]) / 2) // loop back to from last to first point
+        , 0));
+
+
+/**
  * Toggles a specified class for all children based on the predicate return (true: enable, false: disable)
  * @param selector 
  * @param className 
