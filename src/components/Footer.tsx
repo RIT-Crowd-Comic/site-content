f76f64a63ff63e15900a9b08da6bfe08c5eaa36e
YuLingDong <yd4846@rit.edu>
2024-07-26 15:37:53 -0400

    Fixed Comic Link in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,65 +1,70 @@
+'use client'
+
+import { getTrunks} from '@/api/apiCalls';
+import { useRouter } from 'next/navigation';
+
 import Image from "next/image";
 import Link from 'next/link'
-// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 import linkedin from "../../public/images/icons/LinkedIn.svg";
-// import facebook from "../../public/images/icons/Facebook.svg";
 import github from "../../public/images/icons/Github.svg"
 
 const Footer = () => {
+    const router = useRouter();
+
+    const getTrunkUrl = async () => {
+        const trunks = await getTrunks();
+        if (!trunks) return '/';
+        const psID = trunks[0]?.id;
+        if (!psID) return '/';
+        console.log(`url: ${psID}`);
+        return `/comic?id=${psID}`;
+    };
+
     const current_year = new Date().getFullYear();
+
     return (
         <div className={`container-fluid pt-3 bg-dark footer-bar `}>
-            {/* ${styles.footerBar} */}
-            {/* <div className="footer-base"> */}
-            {/* Links */}
-            {/* <div className="row"> */}
-                {/* <div className="col-auto"> */}
-                    <div className="footer-nav">
-                        <div className="footer-head">
-                            <Link href="/">Crowd Comic</Link>
-                        </div>
-                        <ul className="footer-nav-list">
-                            <li><Link href="/">Home</Link></li>
-                            <li><Link href="/team" >Team</Link></li>
-                            <li><Link href="/comic">Comic</Link></li>
-                        </ul>
-                    </div>
-                {/* </div> */}
-                <div className="social-div">
-                    <ul className="social">
-                        <li>
-                            <Link href="">
-                                <div className="icondiv">
-                                    <Image className="iconcolor" src={linkedin} alt="LinkedIn" fill={true} />
-                                </div>
-                            </Link>
-                        </li>
-                        {/* <li>
-                            <Link href="">
-                                <div className="icondiv">
-                                    <Image className="iconcolor" src={facebook} alt="Facebook" fill={true} />
-                                </div>
-                            </Link>
-                        </li> */}
-                        <li>
-                            <Link href="https://github.com/RIT-Crowd-Comic">
-                                <div className="icondiv">
-                                    <Image className="iconcolor" src={github} alt="GitHub" fill={true} />
-                                </div>
-                            </Link>
-                        </li>
-                    </ul>
+            <div className="footer-nav">
+                <div className="footer-head">
+                    <Link href="/">Crowd Comic</Link>
                 </div>
-            {/* </div> */}
-            {/* /Links */}
+                <ul className="footer-nav-list">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/team" >Team</Link></li>
+                    <li><Link 
+                        id="comicLink"
+                        href=""
+                        onClick={async (e) => {
+                            e.preventDefault();
+                            const url = await getTrunkUrl();
+                            router.push(url);
+                        }}
+                        >Comic
+                    </Link></li>
+                </ul>
+            </div>
+            <div className="social-div">
+                <ul className="social">
+                    <li>
+                        <Link href="">
+                            <div className="icondiv">
+                                <Image className="iconcolor" src={linkedin} alt="LinkedIn" fill={true} />
+                            </div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="https://github.com/RIT-Crowd-Comic">
+                            <div className="icondiv">
+                                <Image className="iconcolor" src={github} alt="GitHub" fill={true} />
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
             <div className='copyright-div'>
                 <p className='copyright'>Copyright © {current_year}</p>
             </div>
-            {/* Socials */}
             <div className="row"></div>
-            {/* /Socials */}
-            {/* </div> */}
         </div>
     )
 }
