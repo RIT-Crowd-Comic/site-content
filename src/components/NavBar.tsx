bc2f3134f4e4eaa03224804d12e7748aad13d180
nca6109 <nca6109@g.rit.edu>
2024-08-02 10:13:22 -0500

    Remove console log

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,7 +24,6 @@ const NavBar = () => {
             }
 
             // If not signed in, redirect from user locked pages
-            console.log(window.history);
             const url = window.location.href;
             if (url.includes('/publish')) window.history.length > 2 ? await window.history.go(-1) : window.location.href = '/comic';
             if (url.includes('/profile')) window.history.length > 2 ? await window.history.go(-1) : window.location.href = '/';
