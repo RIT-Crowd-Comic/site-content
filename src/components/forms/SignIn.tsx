5ed96412518547b623d4be7735ca93ee3db008a3
William Tallarico <wct9667@rit.edu>
2024-08-06 16:34:28 -0400

    stuff

--- src/components/forms/SignIn.tsx
+++ src/components/forms/SignIn.tsx
@@ -8,14 +8,51 @@ import logo from '../../../public/images/logos/Crowd_Comic_Logo_BW.svg';
 
 import { useState } from 'react';
 import { loginAction } from '@/app/login/actions';
-
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
 export function SignInForm() {
     const [message, errorState] = useState('');
     const [passwordVisible, setPasswordVisibility] = useState(false);
+    const [validated, setValidated] = useState(false);
+    const [emailValid, setEmailValid] = useState(true);
+    const [emailValidMessage, setEmailValidMessage] = useState('');
+    const [passwordValid, setPasswordValid] = useState(true);
+    const [passwordValidMessage, setPasswordValidMessage] = useState('');
 
     const togglePasswordVisibility = () => {
         setPasswordVisibility(!passwordVisible);
+    };
+
+    const handleSubmit = (event: any) => {
+        if (emailValid === false || passwordValid === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    const handleEmailChange = (e: any) => {
+        const { value } = e.target;
+        const isValid = value.includes('@') &&  value.includes('.');
+        const message = 'Invalid email.';
+        setEmailValidMessage(message)
+        console.log(isValid)
+        setEmailValid(isValid);
+        setValidated(true);
+    };
+
+    // Handler to validate password input
+    const handlePasswordChange = (e: any) => {
+        const { value } = e.target;
+        console.log(value)
+        const isValid = value.length >= 6;
+        console.log(isValid)
+        setPasswordValid(isValid);
     };
 
     return (
@@ -28,7 +65,10 @@ export function SignInForm() {
                     </div>
                 </Link>
                 {/* FORM */}
-                <form
+                <Form
+                    noValidate
+                    validated={validated}
+                    onSubmit={handleSubmit}
                     id={styles.loginForm}
                     action={async (formData) => {
                         const response = await loginAction(formData);
@@ -41,50 +81,61 @@ export function SignInForm() {
                 >
                     <h1 className={styles.h1}>Sign In</h1>
                     {/* EMAIL */}
-                    <div className={`mb-3 ${styles.formInputs}`}>
-                        <label htmlFor="inputEmail" className={styles.loginLabel}>Email Address</label>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="name@example.com"
-                            className="form-control"
-                            id={styles.inputEmail}
-                            aria-describedby="emailHelp"
-                            onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('Enter Email Here')}
-                            onInput={e => (e.target as HTMLInputElement).setCustomValidity('')}
-                            required
-                        />
-                    </div>
-                    {/* PASSWORD */}
-                    <div className={`mb-3 ${styles.formInputs}`}>
-                        <label htmlFor="inputPassword" className={styles.loginLabel}>Password</label>
-                        <div className={styles.passwordContainer}>
-                            <input
-                                type={passwordVisible ? 'text' : 'password'}
-                                name="password"
-                                placeholder="********"
-                                className="form-control"
-                                id={`${styles.inputPassword}`}
-                                onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('Enter Password Here')}
-                                onInput={e => (e.target as HTMLInputElement).setCustomValidity('')}
+                    <Row className={`mb-3 ${styles.formInputs}`}>
+                        <Form.Group>
+                            <Form.Label className={styles.loginLabel}>Email Address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                name="email"
+                                placeholder="name@example.com"
+                                id={`${styles.inputEmail}`}
+                                aria-describedby="emailHelp"
                                 required
+                                onChange={handleEmailChange}
+                                className={`${emailValid ? styles.formControlValid : styles.formControlInvalid}`}
                             />
-                            <button
-                                type="button"
-                                className={styles.togglePassword}
-                                onClick={togglePasswordVisibility}
-                                style={{ backgroundImage: `url(${passwordVisible ? '/images/icons/draw-icons/eyeopen.svg' : '/images/icons/draw-icons/eyeclose.svg'})` }}
-                            />
-                        </div>
-                    </div>
+                            {!emailValid && (
+                                <Form.Control.Feedback type="invalid" className={styles.feedback}>
+                                    {emailValidMessage}
+                                </Form.Control.Feedback>
+                            )}
+                        </Form.Group>
+                    </Row>
+                    {/* PASSWORD */}
+                    <Row className={`mb-3 ${styles.formInputs}`}>
+                        <Form.Group>
+                            <Form.Label className={styles.loginLabel}>Password</Form.Label>
+                            <div className={styles.passwordContainer}>
+                                <Form.Control
+                                    type={passwordVisible ? 'text' : 'password'}
+                                    name="password"
+                                    placeholder="********"
+                                    id={`${styles.inputPassword}`}
+                                    required
+                                    onChange={handlePasswordChange}
+                                    className={`${passwordValid ? styles.formControlValid : styles.formControlInvalid}`}
+                                />
+                                <button
+                                    type="button"
+                                    className={styles.togglePassword}
+                                    onClick={togglePasswordVisibility}
+                                    style={{ backgroundImage: `url(${passwordVisible ? '/images/icons/draw-icons/eyeopen.svg' : '/images/icons/draw-icons/eyeclose.svg'})` }}
+                                />
+                                {!passwordValid && (
+                                    <Form.Control.Feedback className={styles.feedback}>
+                                        Password must be at least 8 characters and contain an uppercase letter.
+                                    </Form.Control.Feedback>
+                                )}
+                            </div>
 
+                        </Form.Group>
+                    </Row>
                     {/* LOGIN */}
                     <button type="submit" id={styles.loginButton} className="btn btn-primary">Sign In</button>
 
                     {/* REGISTER */}
                     <Link href="sign-up" replace={true}><button type="button" id={styles.registerButton} className="btn btn-primary">Sign Up</button></Link>
-                    {!!message && <p>{message}</p>}
-                </form>
+                </Form>
             </section>
         </main>
     );
