81405f1c57de220661ae3dfe0eba00bb9e9a0e51
varpu1 <varpu19@gmail.com>
2024-08-07 00:11:17 -0700

    cleaning up loader code

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -5,15 +5,6 @@ import React, { useEffect, useState } from 'react';
 
 const Loader = ({ show }:{show:boolean}) => {
 
-    // const [showLoader,setShowLoader]=useState(true)
-
-
-    // useEffect(()=>{
-
-    //     //   setShow(true)
-    //       show=false
-
-    // },[])
     return (
         <div style={show ? { display: 'block' } : { display: 'none' }} className={styles.loadContainer}>
             <div className={styles.glowRing} />
