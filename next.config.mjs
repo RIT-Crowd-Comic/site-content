c502238a52ff58c9c743961055048e052d0fb958
apowers131 <arthurpow131@gmail.com>
2024-08-08 08:16:05 -0700

    added s3 image hostname to accepted image remotes

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,17 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'localhost', //update to s3 database instead of local when ready
-            port: '5000',
-          },
+            {
+                protocol: 'http',
+                hostname: 'localhost', // update to s3 database instead of local when ready
+                port:     '5000',
+            },
+            {
+                protocol: 'http',
+                hostname: 'http://rit-igm-crowd-comic-s3-bucket.s3.amazonaws.com/',
+            }
         ],
-      },
+    },
 };
 
 export default nextConfig;
