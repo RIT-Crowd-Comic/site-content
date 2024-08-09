b8502f7073b901a52f7396e8ba4838cbd1af0f44
varpu1 <varpu19@gmail.com>
2024-08-09 06:48:08 -0700

    integrating key component into info box

--- src/components/info/InfoBox.tsx
+++ src/components/info/InfoBox.tsx
@@ -1,18 +1,42 @@
 import styles from './InfoBox.module.css';
 import React, { ReactNode } from 'react';
+import { KeyPoint } from '../interfaces';
+import Key from './Key';
 
 interface Props {
-    text: string,
+    text: (string|KeyPoint[])[],
     visible: boolean
     setVisibility: (b: boolean) => void,
 
 }
 const InfoBox = ({ text, visible, setVisibility }: Props) => {
+
+    const content = document.createElement('div');
+    content.className = styles.modalContent;
+    content.innerHTML = '<span className={styles.closeModal} onClick={() => setVisibility(false)} />';
+    for (const line of text) {
+        if (typeof line == 'string') {
+            const txt = document.createElement('p');
+            txt.innerHTML;
+        }
+    }
     return (
         <div id={styles.infoModal} style={{ display: visible ? 'block' : 'none' }} className={styles.modal}>
             <div className={styles.modalContent}>
                 <span className={styles.closeModal} onClick={() => setVisibility(false)} />
-                <p className={styles.displayLinebreak}>{text}</p>
+                {
+                    text.map((line)=>{
+                        if (typeof line == 'string') {
+                            return <p className={styles.displayLinebreak}>{line}</p>;
+                        }
+                        else {
+                            console.log(line);
+
+                            // return <Key values={line}></Key>
+                        }
+                    })
+                }
+                {/* <p className={styles.displayLinebreak}>{text}</p> */}
             </div>
         </div>
     );
